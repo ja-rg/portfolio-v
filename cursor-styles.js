82fc79859ab356d3dc0f2584b0717f5f2a4d181d
Alejandro Rosales <ja.rg@lasallistas.org.mx>
2025-02-09 12:37:21 -0600

    Mouse assigns x and y to .glower class

--- cursor-styles.js
+++ cursor-styles.js
@@ -1,6 +1,13 @@
-const injectCursorPosition = ({ x, y }) => {
-	document.documentElement.style.setProperty('--x', Math.round(x));
-	document.documentElement.style.setProperty('--y', Math.round(y));
-}
+document.addEventListener('pointermove', (event) => {
+	const element = event.target.closest(".glower");
+	if (!element) return;
 
-document.body.addEventListener('pointermove', injectCursorPosition)
+	const rect = element.getBoundingClientRect();
+	const x = event.clientX - rect.left;
+	const y = event.clientY - rect.top;
+
+	requestAnimationFrame(() => {
+		element.style.setProperty('--x', Math.round(x));
+		element.style.setProperty('--y', Math.round(y));
+	});
+});
